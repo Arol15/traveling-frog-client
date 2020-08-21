33e67ca03bd7751806304c412de97ff0cc7f5990
Lora Rusinouskaya <lrusinou@MacBook-Pro.local>
2020-08-20 20:23:40 -0400

    pins of different colors and popup shows form if point was not visited

---
+++ src/Pages/DetailsView/LogForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LogForm = () => {
+  return (
+    <div>
+      <form>
+        <input></input>
+        <input></input>
+        <input></input>
+      </form>
+    </div>
+  );
+};
+
+export default LogForm;
--- src/Pages/DetailsView/Map.js
+++ src/Pages/DetailsView/Map.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import config from "../../config";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import LogForm from "./LogForm";
 import "./Map.css";
 
 function Map() {
@@ -16,31 +17,36 @@ function Map() {
   const locations = useLocation();
   const typeid = locations.state.id;
   const [pointsofinterest, setPointsofinterest] = useState([]);
-  const [visited, setVisited] = useState([])
+  // const [visited, setVisited] = useState([])
   const [showPopup, setShowPop] = useState({});
 
   const getPointsofinterest = async () => {
-    const allpoints = await fetch(`${config.baseUrl}/pointsofinterest/${typeid}`);
-    const allpointsdata = await allpoints.json();
-    const email = JSON.parse(localStorage.getItem("data")).user.email
+    // const allpoints = await fetch(`${config.baseUrl}/pointsofinterest/${typeid}`);
+    // const allpointsdata = await allpoints.json();
+    const email = JSON.parse(localStorage.getItem("data")).user.email;
     // console.log(email)
-    const res = await fetch(`${config.baseUrl}/users/visits/${email}/${typeid}`);
+    // const res = await fetch(`${config.baseUrl}/users/visits/${email}/${typeid}`);
+    const res = await fetch(
+      `${config.baseUrl}/pointsofinterest/${typeid}/${email}`
+    );
     const data = await res.json();
-    // console.log(data)
-    return [data, allpointsdata];
+    // console.log(data.pointsofinterest)
+    // return [data, allpointsdata];
+    return data;
   };
   // getPointsofinterest()
   useEffect(() => {
     (async () => {
       const data = await getPointsofinterest();
       //   console.log(data)
-    //   setPointsofinterest(data.pointsofinterest);
-      setVisited(data[0].visits);
-      console.log(data[1])
-      setPointsofinterest(data[1].pointsofinterest)
+      //   setPointsofinterest(data.pointsofinterest);
+      // setVisited(data[0].visits);
+      // console.log(data[1])
+      // setPointsofinterest(data[1].pointsofinterest)
+      setPointsofinterest(data.pointsofinterest);
     })();
   }, []);
-console.log(visited);
+  // console.log(visited);
   return (
     <div className="map-container">
       <ReactMapGL
@@ -55,12 +61,11 @@ console.log(visited);
               <div
                 onClick={() =>
                   setShowPop({
-                    ...showPopup,
                     [point.id]: true,
                   })
                 }
               >
-                <img
+                {/* <img
                   className="marker"
                   style={{
                     height: `${6 * viewport.zoom}px`,
@@ -68,47 +73,82 @@ console.log(visited);
                   }}
                   src="https://i.imgur.com/y0G5YTX.png"
                   alt="marker"
-                />
+                /> */}
+                {point.visited ? 
+                (<svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="green"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-map-pin"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>)
+                : ((<svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="32"
+                  height="32"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="red"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-map-pin"
+                >
+                  <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                  <circle cx="12" cy="10" r="3"></circle>
+                </svg>))
+                }
               </div>
-              {/* <svg className='marker'
-            style={{
-                width: "24px", 
-                height: "24px"
-            }}
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
-              <circle cx="12" cy="10" r="3"></circle>
-            </svg> */}
             </Marker>
-            {showPopup[point.id] ? (
+            {(point.visited && showPopup[point.id] ? (
               <Popup
                 latitude={point.lat}
                 longitude={point.lng}
                 closeButton={true}
                 closeOnClick={false}
-                onClose={() => this.setState({ showPopup: false })}
+                dynamicPosition={true}
+                onClose={() => setShowPop({})}
                 anchor="top"
               >
                 <div>
                   <h3>{point.title}</h3>
                   <div>
-                      <img src={point.images} alt='pic' />
+                    <img src={point.images} alt="pic" />
                   </div>
                   <div>
-                      Visited: {point.start_date_visited} {point?.end_date_visited}
+                    Visited on:
+                    {new Date(point.start_date_visited).toLocaleDateString()}
                   </div>
                   <div>
-                      <p>Rating: {point.rating}</p> 
+                    <p>Rating: {point.rating}</p>
                   </div>
-                
                 </div>
               </Popup>
-            ) : null}
+            ) : null) ||
+              (!point.visited && showPopup[point.id] ? (
+                <Popup
+                  latitude={point.lat}
+                  longitude={point.lng}
+                  closeButton={true}
+                  closeOnClick={false}
+                  dynamicPosition={true}
+                  onClose={() => setShowPop({})}
+                  anchor="top"
+                >
+                  <div>
+                    <h3>{point.title}</h3>
+                    <LogForm />
+                  </div>
+                </Popup>
+              ) : null)}
           </>
         ))}
       </ReactMapGL>
