ec171d1f06234a42353757608b539fbe51c965c6
Lora Rusinouskaya <lrusinou@MacBook-Pro.local>
2020-08-20 23:39:49 -0400

    form to log a visit

--- src/Pages/DetailsView/LogForm.js
+++ src/Pages/DetailsView/LogForm.js
@@ -1,12 +1,17 @@
 import React from "react";
+import './LogForm.css'
 
 const LogForm = () => {
   return (
     <div>
-      <form>
-        <input></input>
-        <input></input>
-        <input></input>
+      <form className="logform">
+        <label htmlFor="image">Image</label>
+        <input name="image" type="file" />
+        <label htmlFor="visitDate">Visit Date</label>
+        <input name="visitDate" type='date'/>
+        <label htmlFor="rating">Rating</label>
+        <input name="rating" placeholder="Enter from 1 to 10"/>
+        <button>Submit</button>
       </form>
     </div>
   );
