aa904abe2890b73982534390dba7ffb60a3748dc
Lora Rusinouskaya <lrusinou@c-73-56-9-147.hsd1.fl.comcast.net>
2020-09-29 21:19:57 -0400

    added new URL for heroku Url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const config = {
-    baseUrl: "http://localhost:5000/api", //no trialing slash here
+    baseUrl: "https://traveling-frog.herokuapp.com/api"
+    // baseUrl: "http://localhost:5000/api", //no trialing slash here
   };
   export default config;
